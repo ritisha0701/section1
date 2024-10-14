45b3bcc234a7f386d4abe405404fa14325466191
ritisha0701 <chaurasiaritisha7@gmail.com>
2024-10-14 17:31:04 +0530

    js basic and programs

---
+++ javascript/condition.js
@@ -0,0 +1,40 @@
+num = 234;
+
+if(num%2 ==0){
+    console.log('even');   
+}else{
+    console.log("odd"); 
+}
+
+//wap to check if person is eligible for dl
+age =20;
+
+if(age>=18){
+
+    abc='something';
+    let a = 'local';
+    console.log(a);
+    const pi = 3.14;
+    //pi = 3.15;
+    
+    console.log('Eligible for DL');    
+}else{
+    console.log('Not Eligible for DL');   
+}
+
+console.log(abc);
+//console.log(a);
+
+//WAP to check if a number positive, negative or zero
+
+n = 20;
+if (n>0) {
+    console.log('Positive');    
+}
+else if (n<0){
+    console.log('Negative');
+}
+else{
+    console.log('Zero');
+    
+}
--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
 console.log('Hello JavaScript');
+
+a=234;
+
+console.log(a);
+
+myname = 'Ritisha Chaurasia';
+
+console.log(myname);
+
+console.log(typeof myname);
+console.log(typeof a);
+
+price = 34.24;
+
+console.log(typeof price);
+
+ console.log(1+1);
+ console.log('1'+'1'+'abc');
+ console.log('1'+1);
+ console.log('1'+true);
+ console.log('1'*1+2);
+ 
+ console.log( 1==1 );
+ console.log('1'==1); //== is used to check the value
+ console.log('1'===1); //=== is used to check the value and the type
